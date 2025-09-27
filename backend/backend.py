57abf9ef4c4d8536b2302ccdcd967f594e3e9d68
Soham-Mahale <sohammahale5509@gmail.com>
2025-09-27 16:40:56 +0530

    login signup backend

---
+++ backend/backend.py
@@ -0,0 +1,323 @@
+from fastapi import FastAPI,Path,HTTPException,Query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel,Field,computed_field,field_validator,model_validator
+from typing import Annotated,Literal,Optional
+import json
+
+from pymongo import MongoClient
+from bson import ObjectId   
+import pickle
+import numpy as np
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
+
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+app=FastAPI()
+
+# another pc access
+origins = [
+    "http://localhost:5173",  # Vite React frontend
+    "http://192.168.29.244:5173",  # If you access via LAN
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],           # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],           # Allow all headers
+)
+
+
+
+# utilities
+def load_data()->list[dict]:
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["stress_management_system"]
+    collection = db["users"]
+    data = collection.find()
+    return list(data)
+
+def save_data(data:list[dict]):
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["stress_management_system"]
+    collection = db["users"]
+    if len(data)==7:
+        collection.insert_one(data)
+    else:
+        collection.delete_many({})
+        collection.insert_many(data)
+        
+
+
+#new patient creation and get patient details
+class Patient(BaseModel):
+    name:Annotated[str,Field(...,description="Name of patient",examples=['Soham Mahale'])]
+    mobile_no:Annotated[int,Field(...,description="Mobile number of patient",examples=[9876543210])]
+    dob:Annotated[str,Field(...,description="Date of Birth of patient",examples=['2000-01-01'])]
+    gender:Annotated[str,Literal[Field(...,description='Gender of patient',examples=['Male','Female','Other'])]]
+    email:Annotated[str,Field(None,description="Email of patient",examples=['soham@example.com'])]
+    password:Annotated[str,Field(...,min_length=6,description="Password of patient",examples=['password123'])]
+
+    @field_validator('mobile_no')
+    @classmethod
+    def mobile_validator(cls,value):
+        if len(str(value))==10:
+
+            data=load_data()
+
+            for patient in data:
+                if patient.get("mobile_no")==value:
+                    
+                    raise HTTPException(status_code=400,detail="Mobile number already exists")
+
+            return value
+        raise HTTPException(status_code=400,detail="Mobile number must be 10 digits")
+
+    @field_validator('email')
+    @classmethod    
+    def email_validator(cls,value):
+        value=value.lower()
+        value=value.strip()
+        if "@" not in value:
+            raise HTTPException(status_code=400,detail="Invalid email address")
+        return value
+
+    @computed_field
+    @property
+    def age(self)->int:
+        from datetime import datetime
+        dob=datetime.strptime(self.dob,"%Y-%m-%d")
+        today=datetime.today()
+        age=today.year-dob.year
+
+        return age
+
+class Patient_features(BaseModel):
+    # mental health features
+    anxiety:Annotated[int,Field(...,ge=0,le=10,examples=[5])]
+    self_esteem:Annotated[int,Field(...,ge=0,le=10,examples=[6])]
+    mental_health_history:Annotated[int,Field(...,ge=0,le=1,examples=[1])]
+    depression:Annotated[int,Field(...,ge=0,le=10,examples=[4])]
+
+    # health features
+    headache:Annotated[int,Field(...,ge=0,le=5,examples=[4])]
+    blood_pressure:Annotated[int,Field(...,ge=1,le=3,examples=[3])]
+    sleep_quality:Annotated[int,Field(...,ge=0,le=5,examples=[2])]
+    breathing_problems:Annotated[int,Field(...,ge=0,le=5,examples=[1])]
+    
+    #environmental features
+    noise_level:Annotated[int,Field(...,ge=0,le=5,examples=[3])]
+    living_condition:Annotated[int,Field(...,ge=0,le=5,examples=[4])]
+    safety:Annotated[int,Field(...,ge=0,le=5,examples=[2])]
+    basic_needs:Annotated[int,Field(...,ge=0,le=5,examples=[1])]
+    future_carrer_concern:Annotated[int,Field(...,ge=0,le=5,examples=[3])]
+    social_support:Annotated[int,Field(...,ge=0,le=3,examples=[3])]
+    peer_pressure:Annotated[int,Field(...,ge=0,le=5,examples=[2])]    
+    extracuricular_activities:Annotated[int,Field(...,ge=0,le=5,examples=[4])]
+    bullying:Annotated[int,Field(...,ge=0,le=5,examples=[2])]
+
+    @model_validator(mode='after')
+    @classmethod
+    def update_variables(cls,values):
+
+        values.anxiety=values.anxiety*2
+        values.self_esteem=values.self_esteem*3
+        values.depression=int(values.depression*2.5)
+        print(values)
+        return values
+
+class Update_Patient(BaseModel):
+    name:Optional[Annotated[str,Field(None,description="Name of patient",examples=['Soham Mahale'])]]
+    mobile_no:Optional[Annotated[int,Field(None,description="Mobile number of patient",examples=[9876543210])]]
+    dob:Optional[Annotated[str,Field(None,description="Date of Birth of patient",examples=['01-01-2000'])]]
+    gender:Optional[Annotated[str,Field(None,description='Gender of patient',examples=['Male','Female','Other'])]]
+    email:Optional[Annotated[str,Field(None,description="Email of patient",examples=['soham@example.com'])]]
+    password:Optional[Annotated[str,Field(None,min_length=6,description="Password of patient",examples=['password123'])]]
+
+    @field_validator('mobile_no')
+    @classmethod    
+    def mobile_validator(cls,value):
+        if len(str(value))!=10:
+            raise HTTPException(status_code=400,detail="Mobile number must be 10 digits")
+        return value
+    
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        value=value.lower()
+        value=value.strip()
+        if "@" not in value:
+            raise HTTPException(status_code=400,detail="Invalid email address")
+        return value
+    
+    @computed_field
+    @property
+    def age(self)->int:
+        from datetime import datetime
+        dob=datetime.strptime(self.dob,"%d-%m-%Y")
+        today=datetime.today()
+        age=today.year-dob.year
+
+        return age
+    
+
+
+@app.post("/create_patient")
+def create_patient(patient: Patient):
+    data=patient.model_dump()
+
+    save_data(data)
+    if "_id" in data:
+        data["_id"] = str(data["_id"])
+
+    key=['_id','name']
+    result=dict()
+    for item,value in data.items():
+        if item in key:
+            result[item]=value
+
+    return JSONResponse(status_code=201, content=result)
+
+@app.post("/predict_stress_level")
+def predict_stress_level(patient_features: Patient_features):
+
+    model=pickle.load(open('artifacts/model.pkl','rb'))
+    preprocessor=pickle.load(open('artifacts/preprocessor.pkl','rb'))
+
+    features=np.array([list(patient_features.model_dump().values())])
+
+    print(features)
+
+    features=preprocessor.transform(features)
+
+    print(features)
+
+    prediction=model.predict(features)
+
+    stress_level=prediction[0]
+
+    print(stress_level)
+
+    stress_mapping={0:'Low',1:'Moderate',2:'High'}
+    info_prompt=ChatPromptTemplate(
+            [
+                SystemMessage
+                (
+                    content=
+                    f"""->You are an helpful and empathetic mental wellness assistance.\n
+                    ->Your goal is to provide supportive, actionable advice based on a person's stress level.\n
+                    ->There are 3 progessive stress levels:
+                        1 = Small or Minor stress caused by burnout, fatigue, etc.(No need of doctor consultance).\n
+                        2 = Medium or Mediocar level stress(recommend going for doctor consultance).\n
+                        3 = Sever and Large level stress(Strongly recommend doctor consultance).\n
+                    ->On this stress level you have to give soltuions such as mental exercises, breathing exercises etc.To help and support the person in stress and to remove his/her stress.Dont forget to recommend the doctor consultancy when required.\n
+                    ->Provide suggestion in clear, number list on given stress level only.Keep your tone warm and encouraging.\n
+                    ->Provide the suggestion in json format with key as 'suggestions' and value as list of suggestions.\n
+                      example:{{"breathing_exercises":["suggestion1","suggestion2","suggestion3"]}}
+                    ->Never ever suggest any medicine or any other things.
+                    ->Suggestions must be related to the stress level only.\n
+                    ->Suggest 8to 10 solutions to the patient.\n  
+                    """
+                ),
+                HumanMessage(
+                    content=f"""The stress level of patient={stress_level}.\n
+                    Here are some input that describe patient well{patient_features}.\n
+                    Use this features and give suggetions on this conditions.
+                    Suggest the solutions to the patients on the given stress level only.\n
+                    """
+                )
+            ]
+        )
+    
+    llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.6,
+    google_api_key=api_key
+    )
+
+    final_prompt=info_prompt.invoke({"stress_level":stress_level})
+
+    response=llm.invoke(final_prompt)
+
+    print(type(response.content))
+
+    print(response.content)
+
+    # suggestions=response.content.format()
+
+    # print(suggestions)
+
+    return JSONResponse(status_code=200,content={"stress_level":stress_mapping[stress_level],
+    "suggestions":response.content})
+
+@app.put("/update_patient/{mobile_no}")
+def update_patient(mobile_no: str, updated_patient_data: Patient):
+
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["stress_management_system"]
+    collection = db["users"]
+
+    updated_data=updated_patient_data.model_dump()
+
+    collection.update_one({'mobile_no':int(mobile_no)},{'$set':updated_data})
+
+    return JSONResponse(status_code=200, content=updated_data)
+
+@app.put("/delete_patient/{mobile_no}")
+def delete_patient(mobile_no:int):
+
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["stress_management_system"]
+    collection = db["users"]
+
+    data=list(collection.find())
+    for i in data:
+        if i['mobile_no']==int(mobile_no):
+            collection.delete_one({'mobile_no':int(mobile_no)})
+            break
+
+    return JSONResponse(status_code=200, content={"detail":"Patient deleted successfully"})
+
+@app.post("/login")
+def patient_login(credentials: dict):
+    try:
+        client = MongoClient("mongodb://localhost:27017/")
+        db=client['stress_management_system']
+        collection=db['users']
+
+        if collection.find_one({'mobile_no':int(credentials['mobile_no']),'password':credentials['password']}):
+            data=collection.find_one({'mobile_no':int(credentials['mobile_no']),'password':credentials['password']})
+
+            data['_id']=str(data['_id'])
+            data.pop('password')
+            print(f"data: {data} type: {type(data)} ")
+            return JSONResponse(status_code=200,content={'detail':"Login Sucessful",
+                                                         'data':data})
+
+        return JSONResponse(status_code=401,content={'detail':"Invalid Credentials"})
+    
+    except Exception as e:
+        return JSONResponse(status_code=500,content={'detail':f"Internal Server Error: {e}"})
+
+@app.get("/get_patient/{_id}")
+def get_patient(_id:str=Path(...,description="Unique patient id",examples=["64b8f0f5e1b1c8b5f6d7e9a1"])):
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["stress_management_system"]
+    collection = db["users"]
+    obj_id = ObjectId(_id)
+    data=collection.find_one({"_id":obj_id},{'Password':0})
+    if not data:
+        raise HTTPException(status_code=404,detail="Patient not found")
+
+    data["_id"] = str(data["_id"])
+    
+    return JSONResponse(status_code=200, content=data)
